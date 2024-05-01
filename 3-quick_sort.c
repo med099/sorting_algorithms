4872b74fdc171b17aa6f240da81439976f2cd166
Chaymae Boussetta <boussettachaymae@gmail.com>
2024-05-01 02:39:16 +0100

    task 3

---
+++ 3-quick_sort.c
@@ -0,0 +1,78 @@
+#include <sort.h>
+/**
+*swap - swap the position of two elements
+*@array: array
+*@a: element
+*@b:  element
+*/
+void swap(int *array, ssize_t a, ssize_t b)
+{
+	int tmp;
+
+	tmp = array[a];
+	array[a] = array[b];
+	array[b] = tmp;
+}
+/**
+ *part - sorting scheme implementation
+ *@array: array
+ *@first: first array element
+ *@last: last array element
+ *@size: size array
+ *Return: return the position of the last element sorted
+ */
+int part(int *array, ssize_t first, ssize_t last, size_t size)
+{
+	int pivot = array[last];
+	ssize_t current = first, flag;
+
+	for (flag = first; flag < last; flag++)
+	{
+		if (array[flag] < pivot)
+		{
+			if (array[current] != array[flag])
+			{
+				swap(array, current, flag);
+				print_array(array, size);
+			}
+			current++;
+		}
+	}
+	if (array[current] != array[last])
+	{
+		swap(array, current, last);
+		print_array(array, size);
+	}
+	return (current);
+}
+/**
+ *qs - qucksort algorithm implementation
+ *@array: array
+ *@first: first array element
+ *@last: last array element
+ *@size: array size
+ */
+void qs(int *array, ssize_t first, ssize_t last, int size)
+{
+	ssize_t position = 0;
+
+
+	if (first < last)
+	{
+		position = part(array, first, last, size);
+
+		qs(array, first, position - 1, size);
+		qs(array, position + 1, last, size);
+	}
+}
+/**
+ *quick_sort - prepare the terrain to quicksort algorithm
+ *@array: array
+ *@size: array size
+ */
+void quick_sort(int *array, size_t size)
+{
+	if (!array || size < 2)
+		return;
+	qs(array, 0, size - 1, size);
+}
